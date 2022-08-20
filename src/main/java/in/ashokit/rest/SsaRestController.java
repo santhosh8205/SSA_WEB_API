c127530880aebf2ee15ae9fa1bf8b63b3090b3b7
santhosh <sk245440@gmail.com>
2022-08-20 17:44:29 +0530

    my first commit

---
+++ src/main/java/in/ashokit/rest/SsaRestController.java
@@ -0,0 +1,26 @@
+package in.ashokit.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import in.ashokit.service.SsaService;
+
+@RestController
+public class SsaRestController {
+	
+	@Autowired
+	private SsaService service;
+	
+	@GetMapping("/state/{ssn}")
+	public ResponseEntity<String> stateName(@PathVariable String ssn) {
+		String stateName = service.fetchStateName(ssn);
+		return new ResponseEntity<>(stateName, HttpStatus.OK);
+		
+	}
+	
+
+}
---
+++ src/main/java/in/ashokit/service/SsaService.java
@@ -0,0 +1,28 @@
+package in.ashokit.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SsaService {
+	
+	public String fetchStateName(String ssn) {
+		String stateName = null;
+		
+		if(ssn.startsWith("4")) {
+			stateName = "New Jersey";			
+		}else if (ssn.startsWith("5")) {
+			stateName = "Rhode Island";
+		}else if (ssn.startsWith("6")) {
+			stateName = "Kentuccy";
+		}else if (ssn.startsWith("7")) {
+			stateName = "Ohio";
+		}else {
+			stateName = "Invalid SSN";
+		}
+			
+	return stateName;
+		
+		
+	}
+
+}
